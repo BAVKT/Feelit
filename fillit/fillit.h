0178d2c93fdf92485e9f7c8357bbc5d59f2e66ee
Vincent MERCADIER <vmercadi@e2r11p20.42.fr>
2017-02-07 18:33:09 +0100

    Ajout de la majeur partie des fonctions de resolution

--- fillit/fillit.h
+++ fillit/fillit.h
@@ -15,26 +15,24 @@
 #include "libft.h"
 #include <fcntl.h>
 
-//id = A ou B ou C, etc..
+
 typedef struct 		s_lst
 {
-	int 			hauteur;
-	int 			largeur;
-	int 			ok;
-	char			*piece;
-	char			id;
-	struct s_lst	*next;
+	int 			ok; 			//Si on a deja essayé de la placer.
+	char			*piece; 		//La str de la piece 
+	char			id; 			//La lettre
+	struct s_lst	*next; 			//Le prochain maillon
 }					t_lst;
 
 //Le cube et ses index pour evier d'avoir a les passer en parametrs a chaque fois.
 typedef struct 		s_cube
 {
 	char 			**c;
-	int 			i;
-	int 			j;
+	int 			i;				//Index 1
+	int 			j;				//Index 2
+	int 			size; 			//La taille du cube
 }					t_cube;
-
-t_cube				g_cube;
+t_cube				g_c;
 
 //Pour ranger la forme la plus petite trouvee
 typedef struct 		s_stock
@@ -42,12 +40,22 @@ typedef struct 		s_stock
 	int		taille;
 	char	**bestcube;
 }					t_stock;
+t_stock				g_stock;
 
+int		maincheck(char *av);
 int 	checkforme(char *ligne);
 int		check(char *av);
-int		maincheck(char *av);
+int 	checknl(char *lignenl);
+int 	checkstartend(char *ligne, int *i, int nbl);
 char 	*strreplace(char *str, char c, char d);
 char	*instr(char	*av);
 t_lst	*separe(char *str);
+void	ft_rempid(t_lst *lst);
+void	ft_resetok(t_lst *lst);
+void	ft_isgud(char **tmp);
+int 	ft_capasse(t_lst lst, char **tmp);
+int 	ft_chercheplace(t_lst lst, char **tmp);
+void	ft_testordre(t_lst *lst);
+int		mainres(t_lst *lst);
 
 #endif
--- fillit/format.c
+++ fillit/format.c
@@ -12,147 +12,138 @@
 
 #include "fillit.h"
 
-//Pour rentrer la piece dans le tableau et passer a la suite 
-int 	ft_placement(t_lst lst, t_cube c)
+/*
+**
+** PEUT ETRE QU'IL FAUT METTRE DES POINTS A LA PLACE DES FLECHES
+** OU INVERSEMENT COMME ON PASSE UN MAILLON DE LA STRUCT
+**
+*/
+
+//Fonction pour remplir l'ID de chaque piece
+void 	ft_rempid(t_lst *lst)
 {
-	
+	int 	i;
+
+	i = 0;
+	while(lst)
+	{
+		lst->id = 'A' + i;
+		lst = lst->next;
+		i++;
+	}
 }
 
-//Check si ca rentre en verifiant la hauteur et largeur 
-int 	ft_capasse(t_lst lst, t_cube c)
+//Fonction pour reset le OK de chaque maillon a chaque test d'ordre
+void	ft_resetok(t_lst *lst)
+{
+	while (lst)
+	{
+		lst->ok = 0;
+		lst = lst->next;
+	}
+}
+
+//Pour tester si le cube est mieux aue celui eja enregistrer et l'enregistrer.
+void	ft_isgud(char **tmp)
+{
+	if (g_c.size >= g_stock.size)
+		return ;
+	g_stock.size = g_c.size;
+	g_stock.bestcube = tmp;
+}
+
+//Check si la piece rentre et la met dans cube si c'est bon
+int 	ft_capasse(t_lst lst, char **tmp)
 {
 	int i;	//Pour pas modifier les indexs de base 
 	int j;
+	int k; 	//Index pour la piece
+	int nb 	//Le nombre de diezs
 
-	i = c.i;
-	j = c.j;
-	while (lst.largeur > 0)
+	k = 0;
+	nb = 0; 	
+	i = g_c.i;
+	j = g_c.j;
+	tmp[g_c.i][g_c.j] = '#'; 	//Peutetre cette ligne en trop
+	while (lst->piece[k])
 	{
-		if (c.cube[i][j] != '.')
-			return (0);
-		while (lst.hauteur > 0)
+		if (lst->piece[k] == '#')
 		{
-			if (c.cube[i][j] != '.')	
-				return (0);
-			i++;
+			tmp[g_c.i + (k / 6)][g_c.j + (k % 6)] = '#';	//Pas sur des indexs
+			nb++;
 		}
-		j++;
+		k++;
+		if (nb == 4)
+		{
+			ft_isgud(tmp); 		//Comme c'est bon on enregistre le cube
+			return (1);
+		}
 	}
-	ft_placement(lst, cube);
-	return (1);
+	return (0);
 }
 
 //Avance dans le tableau jusqu'a la recherche d'un point
-int 	ft_chercheplace(t_lst lst, t_cube c)
+int 	ft_chercheplace(t_lst lst, char **tmp)
 {
-	while (c.cube[c.i][c.j] != '.')
+	while (tmp[g_c.i][g_c.j] != '.' && tmp[g_c.i][g_c.j])
 	{
-		if (c.cube[c.i][c.j] == '/n')
+		if (tmp[g_c.i][g_c.j] == '/n')
 		{
-				i++;
-				j = 0;
+				g_c.i++;
+				g_c.j = 0;
 		}
-		else 
-			j++;
+		else
+			g_c.j++;
 	}
-	if (ft_capasse(lst, c) == 0)	//On rappelle la fonction dans qu'on sait pas 
-		ft_chercheplace(lst, c);
+	//Peut etre besoin de faire une incrementation de j pour pas boucler sur le meme point
+	if (ft_capasse(lst, tmp) == 0 && g_c.j != g_c.size)	//On rappelle tant qu'on sait pas 
+		ft_chercheplace(lst, tmp);
+	if (g_c.j == g_c.size - 1)	//Si on est a la fin du cube
+	{
+		//Fonction d'agrandissement du cube
+	}		
 	return(1);
 }
 
-//Le main de resolution
-int		mainres(t_lst lst)
+//La boucle pour tester avec chaque ordre de pieces.
+void	ft_testordre(t_lst *lst)
 {
-	t_cube 	c;
+	char 	**tmp;
+	int 	len;
+	int 	i;
+	int 	j;
+
+	i = 0;
+	j = 0;
+	tmp = g_c.cube;
+	len = ft_lenlst(lst);
+	while (i++ < ((len * len) - len)) //Pas sur du calcul là
+	{
+		while (lst && j++ < len)
+		{
+
+		}
+		lst->ok = 1;
+	}
+	ft_chercheplace(lst)
+}
+
+//Le main de resolution
+int		mainres(t_lst *lst)
+{
 	/*
 	cube.cube = //on y met le tableau créé par jcatot
 	*/
-	c.i = 0;
-	c.j = 0;
-	if (ft_chercheplace(lst, c))
+	g_c.i = 0;
+	g_c.j = 0;
+	ft_resetok(t_lst *lst);
+	if (ft_chercheplace(lst))
 		return (1);
 	return (0);
 }
 
 
 
-
-
-
-
-
-/*
-//Uen fonction qui check si y a des lignes juste de points et qui decale ces lignes 
-// en dessous si elles sont au dessus et a droite si elles sont a gauche.
-//Mais il faut qu'elles ne contiennnt que des '.'
-
-void	ft_formate(t_lst *lst)
-{
-	int 	i;
-	int 	j;
-	int 	ok;
-	char 	**tab;
-
-	i = 0;
-	j = 0;	
-	ok = 0;
-	tab = ft_strsplit(lst->piece, '\n');
-	while (j++ < 4 && ok == 0)
-	{
-		while (i++ < 4 && ok == 0)
-		{
-			if (tab[i][j] == '#')
-				ok = 1;
-		}
-	}
-	while (lst->piece[(i-- * j) - 1] == '.') //On recule jusqu'au \n
-		nb++;
-}
-
-//Decale les lignes
-int	ft_decaligne(t_lst *lst)
-{
-	int 	nbl;	//Numero de la ligne a decaler
-	int 	i;
-
-	nbl = 0;
-	i = 0;
-	while (tab[i][j] == '.')
-	{
-		i++;
-	}
-	return (nbl);
-}
-
-//Décale les colonnes 
-int	ft_decalcol(t_lst *lst)
-{
-	int 	nbc;	//Numero de la colonne a decaler
-	int 	i;
-	int 	ok;
-
-	nbc = 0;
-	ok = 0;
-	i = 0;
-	while (ok == 0)
-	{
-		nbc++;
-		while (lst->piece[i] != '#')
-		{
-			i+=5;
-			if (i >= 20)
-			{
-				i = 0;
-				break ;
-			}
-			if (i == '#')
-				ok = 1;
-		}
-		i++;
-	}
-	return (nbc);
-}
-*/
 
 
 
